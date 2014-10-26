d13454537a22663bbff693e9c40a42b42b5eced4
Dmytro Piliugin <dmytro.piliugin@gmail.com>
2014-10-26 11:09:08 +0200

    added custom casks

---
+++ googleplaymusicremote.rb
@@ -0,0 +1,10 @@
+class Googleplaymusicremote < Cask
+  version '0.0.4'
+  sha256 '8dee8dd7ba00568b5c0a2f00a737835983536fb1d0ff2d4217a5dcd6d0427832'
+
+  url 'https://github.com/stevenla/GooglePlayMusicRemote/releases/download/0.0.4/GooglePlayMusicRemote.0.0.4.zip'
+  homepage 'https://github.com/stevenla/GooglePlayMusicRemote'
+  license :unknown
+
+  app 'GooglePlayMusicRemote.app'
+end
---
+++ open-in-chrome.rb
@@ -0,0 +1,10 @@
+class OpenInChrome < Cask
+  version '1.0'
+  sha256 'bc43cb283d54bdcb11fb8f98a1ed2e381e6a2dd7801f2b092bf35c0b6a212807'
+
+  url 'https://github.com/suaron/Open-in-Chrome/archive/1.0.zip'
+  homepage 'https://github.com/suaron/Open-in-Chrome'
+  license :unknown
+
+  app 'Open-in-Chrome-1.0/ChromeHelper.app'
+end
